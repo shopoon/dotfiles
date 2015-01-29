26fd20a92683e7d77f86e3ef0fbbdf4c463d8b81
shopoon <smallfox.27@gmail.com>
2015-01-29 14:49:21 +0900

    シェルスクリプト追加

---
+++ dotfileLink.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
